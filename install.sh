3d165e694c7fb5adb44990a4af68705e87f004d7
Isaac Boehman <isaac@boehman.me>
2018-07-07 18:49:39 -0700

    update readme and fix install.sh bug

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for component in "client" "control" "viewer"; do
-  sudo eopkg bi --ignore-safety  $component/fah${component}*.eopkg
-  sudo eopkg it --ignore-safety $component/fah${component}*.eopkg
+  sudo eopkg bi --ignore-safety  $component/pspec.xml
+  sudo eopkg it --ignore-safety fah${component}*.eopkg
 done
